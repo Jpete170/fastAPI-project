cee117ada8cbd15a95065095d7c6a94d7e5e9405
Jpete170 <39613600+Jpete170@users.noreply.github.com>
2022-04-06 13:26:15 +1000

    added routing functionality using jinja2, added a base.html file and index.html file to facilitate this; index.html will be a basic landing page for project; added requirements.txt for future use with Heroku

--- api/main.py
+++ api/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message":"Hello World"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
