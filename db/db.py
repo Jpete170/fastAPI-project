b3e15c189cdef1315ca43c93440491b8383c0546
Jpete170 <39613600+Jpete170@users.noreply.github.com>
2022-04-07 14:04:25 +1000

    added CORS middleware, added a few boilerplate files for future use

---
+++ db/db.py
@@ -0,0 +1,2 @@
+#The future database connection file
+#It will connect to a MongoDB instance via Auth0 authentication
--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 #Commented out since it's not being used currently
 #app.mount("/static", StaticFiles(directory="static"), name="static")
 
+origins =[
+    'http://localhost:8000',
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 templates = Jinja2Templates(directory="templates")
 
