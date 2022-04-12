f224671e1fb0f7afcad601f888766c4da3782c7c
Jackson Peters <jpete170@gmail.com>
2022-04-12 13:47:04 +1000

    Main Api routing completed

--- db/db.py
+++ db/db.py
@@ -1,40 +1,60 @@
 import asyncio
 import json
+from operator import contains
 from prisma import Prisma
 from fastapi.encoders import jsonable_encoder
+from prisma.models import netflix_titles
 
 db = Prisma()
 
-async def init(): #used for testing
-    
-    db.connect()
-    res = db.netflix_titles.find_first(
-        where={
-            'title':
-            {'contains':'Johnson'}
-        }
-    )
-    res_json = jsonable_encoder(res)
-    print(res_json)
-
-    db.disconnect()
-    
-    
-
-async def get_all():
-    
+# get all records in the database
+async def get_all():    
     await db.connect()
     res = await db.netflix_titles.find_many()
     res_json = jsonable_encoder(res)
-    #print(res_json)
+    
 
     await db.disconnect()
     return res_json
 
-async def get_one():
+#search records with multiple parameters
+#will be used to handle a majority of page routing
+async def search(column, query):
     await db.connect()
-    res = await db.netflix_titles.find_first()
+    res = await db.netflix_titles.find_many(
+        take=10,
+        where={
+            column :{
+                'contains': query,
+            }
+        }
+    )
     res_json = jsonable_encoder(res)
     await db.disconnect()
     return res_json
     
+#get item show_id
+#Will be used to generate individual web pages for each individual entry
+async def get_showId(id):
+    await db.connect()
+    res = await db.netflix_titles.find_first(
+        where={
+            "show_id": id
+        }
+    )
+    res_json = jsonable_encoder(res)
+    await db.disconnect()
+    return res_json
+
+async def filter_rating(rating):
+    await db.connect()
+    res = await db.netflix_titles.find_many(
+        where={
+            'rating' :{
+                'equals': rating,
+            }
+        }
+    )
+    res_json = jsonable_encoder(res)
+    await db.disconnect()
+    return res_json
--- routers/movies.py
+++ routers/movies.py
@@ -2,15 +2,50 @@ from db import db
 from fastapi import APIRouter, status
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/api",
+    tags=['films']
+)
 
-@router.get("/films", tags=["films"])
-async def read_films_all():
+@router.get("/films")
+async def get_all_films():
     res = await db.get_all()
     
     return res
 
-@router.get("/film", tags=['films'])
-async def read_films_one():
-    res = await db.get_one()
+#basic search function
+@router.get("/films/search")
+async def query_db(column: str, query: str):
+    res = await db.search(column, query)
+    return res
+
+#return document based on id
+@router.get('/films/shows/{show_id}')
+async def get_show_id(show_id : str):
+    res = await db.get_showId(show_id)
+    
+    return res
+
+#Filter based on types (Movie / TV Show)
+@router.get('/films/{type}')
+async def filter_type(type: str):
+    res = await db.search('type', type)
+    return res
+
+#Filter based on ratings
+@router.get('/films/ratings/{rating}')
+async def filter_rating(rating: str):
+    res = await db.filter_rating(rating)
+    return res
+
+#Filter based on Country
+@router.get('/films/country/{country}')
+async def filter_country(country):
+    res = await db.search('country', country) #placeholder function
+    return res
+
+#Filter based on Release Year
+@router.get('/films/year/{year_published}')
+async def filter_year(year_published):
+    res = await db.search('release_year', year_published) #placeholder function
     return res
