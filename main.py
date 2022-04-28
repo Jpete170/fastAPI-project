b4cb091965e657853c0da6b26cfe3b0867bf6a74
Jackson Peters <jpete170@gmail.com>
2022-04-28 16:05:15 +1000

    attempted to add an icon; added vercel url to CORS

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ origins =[
     'http://localhost',
     'http://localhost:8000',
     'http://localhost:3000/',
-    'http://127.0.0.1:3000/'
+    'http://127.0.0.1:3000/',
+    'https://nuxt-app-olive.vercel.app/'
 ]
 
 app.add_middleware(
