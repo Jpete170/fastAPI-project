3daa7697ddb8aac0fa47783049f1ae39bda4af91
Jackson Peters <jackson1480@hotmail.com>
2025-12-29 12:27:22 +1000

    fix module issue

--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,3 @@
-from database import db
-
-__all__ = ["db"]
+# from database import db
+#
+# __all__ = ["db"]
--- routers/movies.py
+++ routers/movies.py
@@ -1,4 +1,4 @@
-from database import db
+from ..database import db
 from fastapi import APIRouter, status, HTTPException, Response
 import os
 
