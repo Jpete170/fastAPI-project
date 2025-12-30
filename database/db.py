ea355e1ee2370f24d5c2986f7891dd3b2b7167c6
Jackson Peters <jackson1480@hotmail.com>
2025-12-30 10:46:00 +1000

    remove prisma

--- database/db.py
+++ database/db.py
@@ -1,92 +1,91 @@
-from operator import contains
-from fastapi.encoders import jsonable_encoder
-from prisma import Prisma
-
-
-db = Prisma()
-
-# get all records in the database
-async def get_all():    
-    await db.connect()
-    res = await db.netflix_titles.find_many()
-    res_json = jsonable_encoder(res)
-    
-
-    await db.disconnect()
-    return res_json
-
-#search records with multiple parameters
-#will be used to handle a majority of page routing
-async def search(column, query, limit):
-    await db.connect()
-    res = await db.netflix_titles.find_many(
-        take=limit,
-        where={
-            column :{
-                'contains': query,
-            }
-        }
-    )
-    res_json = jsonable_encoder(res)
-    await db.disconnect()
-    return res_json
-    
-#The dedicated filtering options for the Movie / TV Show pages in the frontend
-async def filter_page(Type, column, query, limit):
-    await db.connect()
-    res = await db.netflix_titles.find_many(
-        take=limit,
-        where={
-            'type': Type,
-            column:{
-                'contains': query,
-            }
-        }
-    )
-    res_json = jsonable_encoder(res)
-    await db.disconnect()
-    return res_json
-
-#get item show_id
-#Will be used to generate individual web pages for each individual entry
-async def get_showId(id):
-    await db.connect()
-    res = await db.netflix_titles.find_first(
-        where={
-            "show_id": id
-        }
-    )
-    res_json = jsonable_encoder(res)
-    await db.disconnect()
-    return res_json
-
-#Filter by ratings
-async def filter_rating(rating, limit):
-    await db.connect()
-    res = await db.netflix_titles.find_many(
-        take=limit,
-        where={
-            'rating' :{
-                'equals': rating,
-            }
-        }
-    )
-    
-    res_json = jsonable_encoder(res)
-    await db.disconnect()
-    return res_json
-
-#Filter by type
-async def filter_type(type, limit):
-    await db.connect()
-    res = await db.netflix_titles.find_many(
-        take=limit,
-        where={
-            'type': type,
-            
-        }
-    )
-    
-    res_json = jsonable_encoder(res)
-    await db.disconnect()
-    return res_json
+# from operator import contains
+# from fastapi.encoders import jsonable_encoder
+#
+#
+# db = Prisma()
+#
+# # get all records in the database
+# async def get_all():
+#     await db.connect()
+#     res = await db.netflix_titles.find_many()
+#     res_json = jsonable_encoder(res)
+#
+#
+#     await db.disconnect()
+#     return res_json
+#
+# #search records with multiple parameters
+# #will be used to handle a majority of page routing
+# async def search(column, query, limit):
+#     await db.connect()
+#     res = await db.netflix_titles.find_many(
+#         take=limit,
+#         where={
+#             column :{
+#                 'contains': query,
+#             }
+#         }
+#     )
+#     res_json = jsonable_encoder(res)
+#     await db.disconnect()
+#     return res_json
+#
+# #The dedicated filtering options for the Movie / TV Show pages in the frontend
+# async def filter_page(Type, column, query, limit):
+#     await db.connect()
+#     res = await db.netflix_titles.find_many(
+#         take=limit,
+#         where={
+#             'type': Type,
+#             column:{
+#                 'contains': query,
+#             }
+#         }
+#     )
+#     res_json = jsonable_encoder(res)
+#     await db.disconnect()
+#     return res_json
+#
+# #get item show_id
+# #Will be used to generate individual web pages for each individual entry
+# async def get_showId(id):
+#     await db.connect()
+#     res = await db.netflix_titles.find_first(
+#         where={
+#             "show_id": id
+#         }
+#     )
+#     res_json = jsonable_encoder(res)
+#     await db.disconnect()
+#     return res_json
+#
+# #Filter by ratings
+# async def filter_rating(rating, limit):
+#     await db.connect()
+#     res = await db.netflix_titles.find_many(
+#         take=limit,
+#         where={
+#             'rating' :{
+#                 'equals': rating,
+#             }
+#         }
+#     )
+#
+#     res_json = jsonable_encoder(res)
+#     await db.disconnect()
+#     return res_json
+#
+# #Filter by type
+# async def filter_type(type, limit):
+#     await db.connect()
+#     res = await db.netflix_titles.find_many(
+#         take=limit,
+#         where={
+#             'type': type,
+#
+#         }
+#     )
+#
+#     res_json = jsonable_encoder(res)
+#     await db.disconnect()
+#     return res_json
--- routers/movies.py
+++ routers/movies.py
@@ -15,81 +15,81 @@ origin = os.environ.get("ORIGIN")
 async def get_all_films(response: Response):
     response.headers['Access-Control-Allow-Origin'] = origin
 
-    res = await db.get_all()
+    # res = await db.get_all()
     
-    return res
+    return "Films"
 
 #basic search function
 @router.get("/films/search")
 async def query_db(response: Response, column: str, query: str, limit: int | None = None, ):
     response.headers['Access-Control-Allow-Origin'] = origin
-    res = await db.search(column, query, limit)
+    # res = await db.search(column, query, limit)
     
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=422, detail="No Results Found.")
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=422, detail="No Results Found.")
 
 #return document based on id
 @router.get('/films/shows/{show_id}')
 async def get_show_id(show_id, response: Response):
    response.headers['Access-Control-Allow-Origin'] = origin
-   
-   res = await db.get_showId(show_id)        
-   if res is not None:
-    return res
-   else:
-        raise HTTPException(status_code=422, detail="Show Not Found.")
+   #
+   # res = await db.get_showId(show_id)
+   # if res is not None:
+   #  return res
+   # else:
+   #      raise HTTPException(status_code=422, detail="Show Not Found.")
 
 #Filter based on types (Movie / TV Show)
 #Used for initial page load
 @router.get('/films/{type}')
 async def filter_type(response: Response, type: str, limit: int | None = None):
     response.headers['Access-Control-Allow-Origin'] = origin
-    res = await db.search('type', type, limit)
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=404, detail="Show Type Not Found.")
+    # res = await db.search('type', type, limit)
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=404, detail="Show Type Not Found.")
 
 #Filter options for above function
 @router.get('/films/{type}/filter')
 async def filter_results(response: Response, type: str, column: str, query: str, limit: int | None = None):
     response.headers['Access-Control-Allow-Origin'] = origin
 
-    res = await db.filter_page(type, column, query, limit)
-
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=404, detail="Show Type Not Found.")
+    # res = await db.filter_page(type, column, query, limit)
+    #
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=404, detail="Show Type Not Found.")
 
 #Filter based on ratings
 @router.get('/films/ratings/{rating}')
 async def filter_rating(rating: str, response: Response):
     response.headers['Access-Control-Allow-Origin'] = origin
-    res = await db.filter_rating(rating)
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=404, detail="Show Ratings Not Found.")
+    # res = await db.filter_rating(rating)
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=404, detail="Show Ratings Not Found.")
 
 #Filter based on Country
 @router.get('/films/country/{country}')
 async def filter_country(response: Response, country: str, limit: int | None = None):
     response.headers['Access-Control-Allow-Origin'] = origin
-    res = await db.search('country', country, limit) #placeholder function
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=404, detail="Country Not Found.")
+    # res = await db.search('country', country, limit) #placeholder function
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=404, detail="Country Not Found.")
 
 #Filter based on Release Year
 @router.get('/films/year/{year_published}')
 async def filter_year(response: Response, year_published, limit: int | None = None):
     response.headers['Access-Control-Allow-Origin'] = origin
-    res = await db.search('release_year', year_published, limit) #placeholder function
-    if res is not None:
-        return res
-    else:
-        raise HTTPException(status_code=404, detail="Release Year Not Found.")
+    # res = await db.search('release_year', year_published, limit) #placeholder function
+    # if res is not None:
+    #     return res
+    # else:
+    #     raise HTTPException(status_code=404, detail="Release Year Not Found.")
